c6785ab3e94e6546c848defd34d67595672ef8b8
Matheus dos Santos Rodrigues <msrodrigues@acadnet.aeudf.br>
2017-07-11 18:34:45 -0300

    Alterações no persistence e no modelo

--- src/main/java/br/com/model/Cidade.java
+++ src/main/java/br/com/model/Cidade.java
@@ -1,16 +1,41 @@
 package br.com.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "city")
 public class Cidade {
 	@Id
 	@GeneratedValue
 	private long id;
+
+	@Column(name = "Name")
 	private String nome;
+
+	@Column(name = "CountryCode")
+	private String codigoPais;
+
+	@Column(name = "District")
+	private String distrito;
+
+	@Column(name = "Population")
+	private int populacao;
 	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\n\nID: " + getId());
+		sb.append("\nNome: " + getNome());
+		sb.append("\nCódigo do país: " + getCodigoPais());
+		sb.append("\nDistrito: " + getDistrito());
+		sb.append("\nPopulação: " + getPopulacao());
+		return sb.toString();
+	}
+
 	public long getId() {
 		return id;
 	}
@@ -25,6 +50,30 @@ public class Cidade {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getCodigoPais() {
+		return codigoPais;
+	}
+
+	public void setCodigoPais(String codigoPais) {
+		this.codigoPais = codigoPais;
+	}
+
+	public String getDistrito() {
+		return distrito;
+	}
+
+	public void setDistrito(String distrito) {
+		this.distrito = distrito;
+	}
+
+	public int getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(int populacao) {
+		this.populacao = populacao;
 	}
 
 }
--- src/test/java/br/com/teste/TestaConexao.java
+++ src/test/java/br/com/teste/TestaConexao.java
@@ -6,8 +6,11 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.TypedQuery;
 
 import br.com.model.Avaliacao;
+import br.com.model.Cidade;
 import br.com.model.ComodidadeEstab;
 import br.com.model.Estabelecimento;
 import br.com.model.Localizacao;
@@ -15,10 +18,32 @@ import br.com.model.Quarto;
 import br.com.model.TipoEstabelecimento;
 
 public class TestaConexao {
+	@PersistenceUnit(name = "HotelBudapeste")
+	static EntityManagerFactory factoryHotel;
+	
+	@PersistenceUnit(name = "HotelBudapeste")
+	static EntityManager managerHotel;
+	
+	@PersistenceUnit(name = "World")
+	static EntityManagerFactory factoryWorld;
+	
+	@PersistenceUnit(name = "World")
+	static EntityManager managerWorld;	
 	public static void main(String[] args) {
-		System.out.println("Criando o banco...");
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("HotelBudapeste");
-		System.out.println("Banco criado");
+		
+		System.out.println("Banco World");
+		factoryWorld = Persistence.createEntityManagerFactory("world");
+		managerWorld = factoryWorld.createEntityManager();
+		
+		String s = "select c from Cidade c where c.id = :id";
+		TypedQuery<Cidade> query = managerWorld.createQuery(s, Cidade.class);
+		query.setParameter("id", new Long(241));
+		Cidade resultado = query.getSingleResult();
+		System.out.println(resultado);
 
+//		System.out.println("Banco hotel...");
+//		factoryHotel = Persistence.createEntityManagerFactory("HotelBudapeste");
+//		
+//		System.out.println("Banco hotel criado");
 	}
 }
